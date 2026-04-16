5ceda9949dfc080993b3190fe6ef06e9a3565642
gianf03 <gianfvitiello03@gmail.com>
2026-04-16 21:12:05 +0200

    aggiunta colab prof e tanto altro...

---
+++ test_modello.py
@@ -0,0 +1,8 @@
+"""
+pipeline:
+
+1. se possibile, aumentare qualità delle immagini con filtri 
+2. individuare frame adeguati in cui i 68 punti sono pienamente visibili
+3. trovare metriche adeguate
+
+"""
